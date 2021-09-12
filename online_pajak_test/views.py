45e44f10f6bb40bbd52d3870a86e0769b9c3a2e8
Virak HOR <viirak@gmail.com>
2021-09-12 22:42:48 +0700

    mod view name and response status

--- online_pajak_test/views.py
+++ online_pajak_test/views.py
@@ -1,14 +1,15 @@
 from statistics import mean
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import InvoiceData
 from .utils import get_diff_days, get_score
 from rest_framework.permissions import AllowAny
 
 
-class CompanyView(APIView):
+class CompanyValidationView(APIView):
     """
-    View to find companies in the system.
+    View to validate a company in the system.
     """
 
     permission_classes = (AllowAny,)
@@ -16,8 +17,6 @@ class CompanyView(APIView):
     def get(self, request, company_name):
         """
         Return a company that exist in the system.
-
-        * requires: company's name
         """
         name = company_name.lower()
         invoices = InvoiceData().get_invoices_by_company_name(name)
@@ -25,7 +24,11 @@ class CompanyView(APIView):
         str_exist = "exists" if is_company_exist else "does not exist"
         msg = "The company name: \"{}\" {} in the system.".format(
           company_name, str_exist)
-        return Response({"exist":is_company_exist, "message":msg})
+        response_status = status.HTTP_200_OK if is_company_exist \
+          else status.HTTP_404_NOT_FOUND
+        return Response(
+          data={"exist": is_company_exist, "message": msg},
+          status=response_status)
 
 
 class CompanyRelationView(APIView):
@@ -64,12 +67,12 @@ class CompanyRelationView(APIView):
                   for i in range(len(invoice_dates)) if (i > 0)]
             average_day_delayed = mean(diff_days)
           score = get_score(invoice_count, average_day_delayed)
-          return Response({
+          return Response(data={
             "score": score,
             "message": "There's relationship between these companies."
-          })
+          }, status=status.HTTP_200_OK)
         else:
-          return Response({
+          return Response(data={
               "score": None,
               "message": "There's no relationship between these companies."
-          })
+          }, status=status.HTTP_404_NOT_FOUND)
