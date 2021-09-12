d5449212a047b48384afc0d1e46aa5655819a16a
Virak HOR <viirak@gmail.com>
2021-09-12 20:56:46 +0700

    added utils

---
+++ online_pajak_test/utils.py
@@ -0,0 +1,17 @@
+import math
+import datetime
+
+def get_diff_days(str_date1, str_date2):
+  d1 = datetime.datetime.strptime(str_date1, "%Y-%m-%d").date()
+  d2 = datetime.datetime.strptime(str_date2, "%Y-%m-%d").date()
+  return abs(d2 - d1).days
+
+def get_score(total, average):
+  """
+  Score starting from 0 and scale using logorithm based 10
+  1 invoice get 1 point. But after x days until next invoice, 
+  point will be decreased by 1
+  """
+  POINT_SCALE = 7 # days (could be changed to any preferred length)
+  average_point = average/POINT_SCALE
+  return round(math.log10(total - average_point), 4) if total > average_point else 0
